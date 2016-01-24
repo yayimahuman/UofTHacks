05ab10b35d59e694da0b89619959e3933ea22485
yayimahuman <donha@utschools.ca>
2016-01-23 21:47:12 -0500

    reorganized code, fixed jquery syntax errors

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,22 +21,13 @@
 
     <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
 </head>
 
 <body id="page-top" data-spy="scroll">
 
     <div id="map"></div>
+
     <div id="menu">
-
-
-
         <div class="col-sm-12">
 
             <h1>Accounted Itinerant</h1>
@@ -46,13 +36,11 @@
             <form id="initial-form" class="form-horizontal" role="form">
 
                 <div id="form-group-1" class="form-group">
-                    <!--<label class="control-label col-sm-2 col-sm-offset-1" for="origin"><i class="fa fa-map-marker"></i></label>-->
                     <div class="col-sm-6 col-sm-offset-3 input-group input-group-lg">
+
                         <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-record" aria-hidden="true"></span></span>
 
                         <input type="text" class="form-control" id="origin" placeholder="Enter your starting point, or leave this blank for current location" aria-describedby="basic-addon1">
-
-
 
                     </div>
                 </div>
@@ -62,35 +50,25 @@
                 <div id="form-group-2" class="form-group">
                     <!--<label class="control-label col-sm-2 col-sm-offset-1" for="destination">Destination:</label>-->
                     <div class="col-sm-6 col-sm-offset-3 input-group input-group-lg">
+
                         <span class="input-group-addon" id="basic-addon2"><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span></span>
 
                         <input type="text" class="form-control" id="destination" placeholder="Enter your destination, or leave this blank for your current location" aria-describedby="basic-addon2">
 
-
-
                     </div>
                 </div>
-
 
                 <div class="clearfix"></div>
 
                 <div id="form-group-3" class="form-group" style="text-align:center">
-                    <!--<label class="control-label col-sm-2 col-sm-offset-1" for="destination">Destination:</label>-->
 
                     <button type="submit" class="btn btn-default btn-lg">Submit</button>
 
-
-
-
-                    </div>
                 </div>
 
             </form>
 
         </div>
-
-
-
     </div>
 
     <!-- jQuery library -->
@@ -99,27 +77,25 @@
     <!-- Latest compiled JavaScript -->
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
-<!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
 
 
 <script>
 $( "#origin" ).focus(function() {
-
+    //add fancy stuff
 });
 $( "#destination" ).focus(function() {
-
+    //add fancy stuff
 });
-
 $( "#initial-form" ).submit(function() {
-    var origin = $('#origin').val();
-    var destination = $('#destination').val(function{
-        $("#menu").remove();//make sure values are collected before removal
+    var origin = $('#origin').val(function(){
+        var destination = $('#destination').val(function(){
+            $("#menu").remove();//make sure values are collected before removal
+        });
     });
-
     //add logic here to dfault to current location when field is blank
 });
-
 
 
 jQuery(document).ready(function () {
